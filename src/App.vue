24078c8b9b282dffcfc46e73b34bbb184a9b5486
chenfangyin <fangyin.chen@gmail.com>
2024-12-22 20:19:21 +0800

    fix: bugs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <el-config-provider namespace="cfy">
-    <!-- <BaseHeader /> -->
     <div class="main-container flex">
-      <!-- <BaseSide /> -->
-      <!-- <div w="full" py="4">
-      </div> -->
       <RouterView />
     </div>
   </el-config-provider>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import MyButton from '~/components/MyButton.vue'
-import '~/styles/index.scss'
 
 export { MyButton }
